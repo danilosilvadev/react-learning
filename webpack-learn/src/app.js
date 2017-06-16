89737464b1dc677b7e533a2c5a39f7ce066d6815
GorickUPM <danilosilvaweb@hotmail.com>
2017-06-16 15:47:52 -0300

    ok

--- webpack-learn/src/app.js
+++ webpack-learn/src/app.js
@@ -1,6 +1,8 @@
-var _ = require('lodash');
+'use strict';
 
-var array = [1];
-var other = _.concat(array, 2, [3], [[4]]);
+import _ from 'lodash'; //ES6 import to check our babel loader
+
+const array = [1];
+const other = _.concat(array, 2, [3], [[4]]);
 
 console.log(other); //[1, 2, 3, [4]]
--- webpack-learn/webpack.config.js
+++ webpack-learn/webpack.config.js
@@ -1,17 +1,51 @@
 var webpack = require('webpack');
+const { join } = require('path');
 
 var config = {
-    context: __dirname + 'src', // `__dirname` is root of project and `src` is source
+    context: join(__dirname, 'src'), // `__dirname` is root of project and `src` is source
     entry: {
         app: './app.js',
     },
     output: {
-        path: __dirname + 'dist', // `dist` is the destination
+        path: join(__dirname, 'dist'), // `dist` is the destination
         filename: '[name].bundle.js',
     }, devServer: {
         open: true, // to open the local server in browser
-        contentBase: __dirname + 'src',
+        contentBase: join(__dirname, 'src'),
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //Check for all js files
+                loader: 'babel-loader',
+                query: {
+                    presets: ["babel-preset-es2015"].map(require.resolve)
+                }
+            }
+        ]
+    },
+    module: {
+        rules: [{
+            test: /\.(sass|scss)$/, //Check for sass or scss file names
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    }, devtool: "eval-source-map" // Default development sourcemap
 };
+
+// Check if build is running in production mode, then change the sourcemap type
+if (process.env.NODE_ENV === "production") {
+    config.devtool = "source-map";
+
+    // Can do more here
+    // JSUglify plugin
+    // Offline plugin
+    // Bundle styles seperatly using plugins etc,
+}
+
 
 module.exports = config;
